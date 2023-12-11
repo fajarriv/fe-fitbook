c03351ef2d8a919ee8f478fefc197a0af26ccef9
fajarriv <rivaldiibnusina@gmail.com>
2023-12-11 11:57:37 +0700

    add color palette and default html tag style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,66 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        montserrat: ["var(--font-poppins)"],
       },
+      fontSize: {
+        "h1-desktop": [
+          "64px",
+          { lineHeight: "64px", letterSpacing: "-0.25px", fontWeight: "700" },
+        ],
+        "h2-desktop": [
+          "45px",
+          { lineHeight: "52px", letterSpacing: "0px", fontWeight: "700" },
+        ],
+        "h3-desktop": [
+          "36px",
+          { lineHeight: "44px", letterSpacing: "0px", fontWeight: "700" },
+        ],
+
+        "h1-mobile": [
+          "48px",
+          { lineHeight: "40px", letterSpacing: "0", fontWeight: "700" },
+        ],
+        "h2-mobile": [
+          "28px",
+          { lineHeight: "36px", letterSpacing: "0", fontWeight: "700" },
+        ],
+        "h3-mobile": [
+          "24px",
+          { lineHeight: "32px", letterSpacing: "0", fontWeight: "700" },
+        ],
+        "p-desktop": [
+          "16px",
+          { lineHeight: "24px", letterSpacing: "+0.15px", fontWeight: "500" },
+        ],
+        "p-mobile": [
+          "14px",
+          { lineHeight: "20px", letterSpacing: "0.25px", fontWeight: "500" },
+        ],
+      },
+      colors: {
+        black: "#0C0404",
+        darkcream: "#EFD7C5",
+        cream: "#F4EEE8",
+        green: "#0C4C4C",
+        orange: "#DE7055",
+
+        greyborder: "#AAAAAA",
+        greyborder: "#C9C9C9",
+
+        success: "#43BF35",
+        error: "#BF0835",
+        warning: "#F4AD43",
+        disabled: "#79747E",
+      },     
     },
   },
   plugins: [],
-}
+};
